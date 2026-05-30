dae7f9377f20dfb0702f027da363d8bbf5b99031
yutouzei <yutouzei@gmail.com>
2026-05-30 19:31:01 +0800

    feat: added transaction support for course schedule

--- services/dbService.js
+++ services/dbService.js
@@ -1,24 +1,33 @@
 const { client } = require('../config/db');
 const { DB_NAME, COLLECTIONS } = require('../config/constants');
 
-// TODO: Add transaction
 async function populateDB(acadYr, sem, courseSchedule) {
+    const session = client.startSession();
+
     try {
+        session.startTransaction();
+
         // store acadYr and sem
         let collection = client.db(DB_NAME).collection(COLLECTIONS.YR_SEM);
         await collection.updateOne(
             {},
             { $set: { acadYr: acadYr, sem: sem } },
-            { upsert: true }
+            { upsert: true, session }
         );
 
         // store course schedule
         collection = client.db(DB_NAME).collection(COLLECTIONS.COURSE_SCHEDULE);
-        await collection.deleteMany({});
-        await collection.insertMany(courseSchedule);
+
+        await collection.deleteMany({}, { session });
+        await collection.insertMany(courseSchedule, { session });
+
+        await session.commitTransaction();
     } catch (err) {
         console.error("Database operation failed:", err.message);
+        await session.abortTransaction();
         throw err;
+    } finally {
+        await session.endSession();
     }
 }
 
